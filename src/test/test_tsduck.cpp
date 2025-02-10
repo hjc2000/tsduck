ae69786a97a7fb6c02d8fa4dd867106467e83242
hjc2000 <hjc.666@foxmail.com>
2025-02-10 16:59:35 +0800

    std::

--- src/test/test_tsduck.cpp
+++ src/test/test_tsduck.cpp
@@ -7,44 +7,41 @@
 #include <tsduck/mux/JoinedTsStream.h>
 #include <tsduck/TestProgramMux.h>
 
-using namespace std;
-using namespace video;
-
 void test_tsduck()
 {
 	// 输入文件
-	base::Queue<string> file_queue;
+	base::Queue<std::string> file_queue;
 	file_queue.Enqueue("不老梦.ts");
 	file_queue.Enqueue("水龙吟.ts");
 	file_queue.Enqueue("idol.ts");
 
-	JoinedTsStream joined_ts_stream;
+	video::JoinedTsStream joined_ts_stream;
 	joined_ts_stream._on_ts_packet_source_list_exhausted = [&]()
 	{
-		string file_name;
+		std::string file_name;
 		if (!file_queue.TryDequeue(file_name))
 		{
 			return;
 		}
 
 		shared_ptr<base::IFileStream> input_file_stream = base::di::filesystem::OpenExisting(file_name.c_str());
-		shared_ptr<TSPacketStreamReader> ts_packet_reader{new TSPacketStreamReader{input_file_stream}};
+		shared_ptr<video::TSPacketStreamReader> ts_packet_reader{new video::TSPacketStreamReader{input_file_stream}};
 		joined_ts_stream.AddSource(ts_packet_reader);
 	};
 
-	shared_ptr<TestProgramMux> test_program_mux{new TestProgramMux{}};
+	shared_ptr<video::TestProgramMux> test_program_mux{new video::TestProgramMux{}};
 	base::CancellationTokenSource cancel_pump_source;
-	ITSPacketSource::ReadPacketResult pump_result = joined_ts_stream.PumpTo(test_program_mux, cancel_pump_source.Token());
+	video::ITSPacketSource::ReadPacketResult pump_result = joined_ts_stream.PumpTo(test_program_mux, cancel_pump_source.Token());
 	switch (pump_result)
 	{
-	case ITSPacketSource::ReadPacketResult::NoMorePacket:
+	case video::ITSPacketSource::ReadPacketResult::NoMorePacket:
 		{
-			cout << "读取完成，没有更多包了" << endl;
+			std::cout << "读取完成，没有更多包了" << std::endl;
 			break;
 		}
 	default:
 		{
-			cout << "其他错误" << endl;
+			std::cout << "其他错误" << std::endl;
 			break;
 		}
 	}
