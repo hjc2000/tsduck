03f5e666766675b393f3a13150177926093c4bba
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-05-13 19:33:21 +0800

    add_library(${ProjectName} STATIC)

---
+++ project.cmake
@@ -0,0 +1,9 @@
+add_library(${ProjectName} STATIC)
+
+# 将资源目录用预定义传给 C++ 程序
+target_compile_definitions(${ProjectName} PRIVATE TS_NO_SRT=1)
+target_compile_definitions(${ProjectName} PRIVATE TS_NO_RIST=1)
+
+target_import_src(${ProjectName})
+target_import_base(${ProjectName} PUBLIC)
+target_link_libraries(${ProjectName} PUBLIC winmm Userenv Ws2_32)
