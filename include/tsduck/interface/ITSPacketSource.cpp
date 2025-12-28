08b59b7a2e63fa4d14eb5ec24c6db1ec638d2daa
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-12-28 22:25:38 +0800

    ``` feat(ITSPacketSource): 更新取消令牌检查方法
    - 添加 base/task/CancellationToken.h 头文件包含
    - 将 CancellationToken 的 IsCancellationRequested 方法调用
      替换为 base::is_cancellation_requested 函数调用
    - 修正头文件包含格式，添加必要的空格
    - 调整代码格式以提高可读性
    ```

--- include/tsduck/interface/ITSPacketSource.cpp
+++ include/tsduck/interface/ITSPacketSource.cpp
@@ -1,33 +1,33 @@
-#include"ITSPacketSource.h"
+#include "ITSPacketSource.h"
+#include "base/task/CancellationToken.h"
 
 using namespace video;
 
 ITSPacketSource::ReadPacketResult ITSPacketSource::PumpTo(
 	shared_ptr<ITSPacketConsumer> consumer,
-	shared_ptr<base::CancellationToken> cancel_pump
-)
+	shared_ptr<base::CancellationToken> cancel_pump)
 {
 	std::vector<shared_ptr<ITSPacketConsumer>> consumers;
 	consumers.push_back(consumer);
 	return PumpTo(consumers, cancel_pump);
 }
 
-ITSPacketSource::ReadPacketResult ITSPacketSource::PumpTo(
-	std::vector<shared_ptr<ITSPacketConsumer>> const consumers,
-	shared_ptr<base::CancellationToken> cancel_pump
-)
+ITSPacketSource::ReadPacketResult ITSPacketSource::PumpTo(std::vector<shared_ptr<ITSPacketConsumer>> const consumers,
+														  shared_ptr<base::CancellationToken> cancel_pump)
 {
 	ts::TSPacket packet;
-	while (!cancel_pump->IsCancellationRequested())
+
+	while (!base::is_cancellation_requested(cancel_pump))
 	{
 		ITSPacketSource::ReadPacketResult read_packet_result = ReadPacket(packet);
+
 		switch (read_packet_result)
 		{
 		case ITSPacketSource::ReadPacketResult::Success:
 			{
 				for (auto consumer : consumers)
 				{
-					if (cancel_pump->IsCancellationRequested())
+					if (base::is_cancellation_requested(cancel_pump))
 					{
 						return ITSPacketSource::ReadPacketResult::Success;
 					}
